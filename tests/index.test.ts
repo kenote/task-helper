392458dd096cbfeaf39e1970ac507d0f89bc9e75
thondery <thondery@163.com>
2019-07-16 16:37:14 +0800

    fix tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,21 +6,21 @@ describe('\nTasks testing ->\n', () => {
 
   test('Default Press Enter', async () => {
     let result: any = await run([cliPath], [ENTER])
-    expect(/(\? 操作类型: initialize)/.test(result)).toBeTruthy()
+    expect(/(\? 操作类型: initialize)/.test(result)).toBe(true)
   })
 
   test('Press Down > Enter', async () => {
     let result: any = await run([cliPath], [DOWN, ENTER])
-    expect(/(\? 操作类型: reset)/.test(result)).toBeTruthy()
+    expect(/(\? 操作类型: reset)/.test(result)).toBe(true)
   })
 
   test('Press Down > Up > Enter', async () => {
     let result: any = await run([cliPath], [DOWN, UP, ENTER])
-    expect(/(\? 操作类型: initialize)/.test(result)).toBeTruthy()
+    expect(/(\? 操作类型: initialize)/.test(result)).toBe(true)
   })
 
   test('Input & Press Enter', async () => {
     let result: any = await run([cliPath], ['3', ENTER])
-    expect(/(\? 操作类型: exit)/.test(result)).toBeTruthy()
+    expect(/(\? 操作类型: exit)/.test(result)).toBe(true)
   })
 })
